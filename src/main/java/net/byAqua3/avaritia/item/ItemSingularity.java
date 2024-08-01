7c11de6f2d927f475a9eccd46df2baa9f76205da
Aqua3 <88130177+AquaThree@users.noreply.github.com>
2024-08-02 00:20:20 +0800

    Update ItemSingularity.java

--- src/main/java/net/byAqua3/avaritia/item/ItemSingularity.java
+++ src/main/java/net/byAqua3/avaritia/item/ItemSingularity.java
@@ -2,15 +2,16 @@ package net.byAqua3.avaritia.item;
 
 import java.awt.Color;
 
-import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
 
-public class ItemSingularity extends Item {
+public class ItemSingularity extends ItemHalo {
 	
 	private Color color;
 	private Color layerColor;
 
 	public ItemSingularity(Properties properties, int color, int layerColor) {
-		super(properties);
+		super(properties, 2);
 		this.color = new Color(color);
 		this.layerColor = new Color(layerColor);
 	}
@@ -21,5 +22,16 @@ public class ItemSingularity extends Item {
 	
 	public Color getLayerColor() {
 		return this.layerColor;
+	}
+	
+	@Override
+	public boolean hasCustomEntity(ItemStack stack) {
+		return true;
+	}
+	
+	@Override
+	public boolean onEntityItemUpdate(ItemStack stack, ItemEntity itemEntity) {
+		itemEntity.setInvulnerable(true);
+		return super.onEntityItemUpdate(stack, itemEntity);
 	}
 }
