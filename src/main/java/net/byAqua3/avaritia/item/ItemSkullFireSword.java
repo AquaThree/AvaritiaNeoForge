cc87f8bcb5c4b9141582cb6decc1b86123895a2b
Aqua3 <88130177+AquaThree@users.noreply.github.com>
2024-08-02 00:19:41 +0800

    Update ItemSkullFireSword.java

--- src/main/java/net/byAqua3/avaritia/item/ItemSkullFireSword.java
+++ src/main/java/net/byAqua3/avaritia/item/ItemSkullFireSword.java
@@ -6,24 +6,28 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 public class ItemSkullFireSword extends SwordItem {
 
-	public ItemSkullFireSword(
-			Properties properties) {
-		super(InfinityTiers.SKULLFIRE, 7, -2.4F, properties);
+	public ItemSkullFireSword(Properties properties) {
+		super(Tiers.DIAMOND, 3, -2.4F, properties);
 	}
-	
+
 	@Override
 	public boolean hasCustomEntity(ItemStack stack) {
 		return true;
 	}
-	
+
+	@Override
+	public int getDamage(ItemStack stack) {
+		return 0;
+	}
+
 	@Override
 	public void appendHoverText(ItemStack stack, Level level, List<Component> tooltip, TooltipFlag flag) {
 		tooltip.add(Component.translatable("tooltip.avaritia.skullfire_sword.desc").withStyle(ChatFormatting.ITALIC).withStyle(ChatFormatting.DARK_GRAY));
 	}
-
 }
