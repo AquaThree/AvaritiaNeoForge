5f69dbc85516e66261ee4dec72a575ad430ec901
Aqua3 <88130177+AquaThree@users.noreply.github.com>
2024-08-02 00:17:06 +0800

    Update EntityGapingVoid.java

--- src/main/java/net/byAqua3/avaritia/entity/EntityGapingVoid.java
+++ src/main/java/net/byAqua3/avaritia/entity/EntityGapingVoid.java
@@ -1,25 +1,39 @@
 package net.byAqua3.avaritia.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import net.byAqua3.avaritia.block.BlockInfinityChest;
 import net.byAqua3.avaritia.loader.AvaritiaEntities;
 import net.byAqua3.avaritia.loader.AvaritiaSounds;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.boss.wither.WitherBoss;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Explosion;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseFireBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
@@ -33,7 +47,9 @@ public class EntityGapingVoid extends Entity {
 	public static final double COLLAPSE = 0.95D;
 
 	private static final int SUCK_RANGE = 20;
-	
+
+	private Player player;
+
 	public EntityGapingVoid(EntityType<?> entityType, Level level) {
 		super(entityType, level);
 		this.noCulling = true;
@@ -51,6 +67,14 @@ public class EntityGapingVoid extends Entity {
 		this.entityData.set(AGE, Integer.valueOf(age));
 	}
 
+	public Player getPlayer() {
+		return this.player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
 	@Override
 	protected void defineSynchedData() {
 		this.entityData.define(AGE, Integer.valueOf(0));
@@ -64,6 +88,17 @@ public class EntityGapingVoid extends Entity {
 	@Override
 	protected void addAdditionalSaveData(CompoundTag tag) {
 		tag.putInt("age", this.getAge());
+	}
+
+	@Override
+	public boolean shouldRenderAtSqrDistance(double distance) {
+		double d0 = 1.0D;
+		if (Double.isNaN(d0)) {
+			d0 = 1.0;
+		}
+
+		d0 *= 64.0 * getViewScale();
+		return distance < d0 * d0;
 	}
 
 	private static double ease(double in) {
@@ -90,7 +125,7 @@ public class EntityGapingVoid extends Entity {
 		}
 		if (entity instanceof Player) {
 			Player player = (Player) entity;
-			return (!player.isCreative() || !player.getAbilities().flying);
+			return !player.isCreative();
 		}
 		return true;
 	}
@@ -106,7 +141,6 @@ public class EntityGapingVoid extends Entity {
 		return true;
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void tick() {
 		super.tick();
@@ -120,89 +154,116 @@ public class EntityGapingVoid extends Entity {
 
 		this.setAge(age + 1);
 
-		if (!this.level().isClientSide()) {
-			Vec3 pos = this.position();
-			double voidScale = getVoidScale(age);
+		Vec3 pos = this.position();
+		double voidScale = getVoidScale(age);
 
-			for (int i = 0; i < 50; i++) {
-				this.level().addParticle(ParticleTypes.PORTAL, this.getX(), this.getY(), this.getZ(),
-						this.random.nextGaussian() * 3.0D, this.random.nextGaussian() * 3.0D,
-						this.random.nextGaussian() * 3.0D);
+		for (int i = 0; i < 50; i++) {
+			this.level().addParticle(ParticleTypes.PORTAL, this.getX(), this.getY(), this.getZ(),
+					this.random.nextGaussian() * 3.0D, this.random.nextGaussian() * 3.0D,
+					this.random.nextGaussian() * 3.0D);
+		}
+
+		double radius = voidScale * 0.5D;
+
+		AABB aabb = AABB.encapsulatingFullBlocks(
+				this.blockPosition().offset(new Vec3i(-SUCK_RANGE, -SUCK_RANGE, -SUCK_RANGE)),
+				this.blockPosition().offset(new Vec3i(SUCK_RANGE, SUCK_RANGE, SUCK_RANGE)));
+		List<Entity> entities = this.level().getEntitiesOfClass(Entity.class, aabb, this::shouldSuck);
+
+		for (Entity entity : entities) {
+			if (entity != this) {
+				double posX = pos.x - entity.getX();
+				double posY = pos.y - entity.getY();
+				double posZ = pos.z - entity.getZ();
+				double len = Math.sqrt(posX * posX + posY * posY + posZ * posZ);
+				double lenn = len / SUCK_RANGE;
+				if (len <= SUCK_RANGE) {
+					double strength = (1 - lenn) * (1 - lenn);
+					double power = 0.075 * radius;
+
+					Vec3 motion = entity.getDeltaMovement();
+
+					double motionX = motion.x() + (posX / len) * strength * power;
+					double motionY = motion.y() + (posY / len) * strength * power;
+					double motionZ = motion.z() + (posZ / len) * strength * power;
+
+					entity.setDeltaMovement(motionX, motionY, motionZ);
+				}
 			}
+		}
 
-			double radius = voidScale * 0.5D;
+		int attackRange = (int) (radius * 0.95D);
 
-			AABB aabb = AABB.encapsulatingFullBlocks(this.blockPosition().offset(new Vec3i(-SUCK_RANGE, -SUCK_RANGE, -SUCK_RANGE)),
-					this.blockPosition().offset(new Vec3i(SUCK_RANGE, SUCK_RANGE, SUCK_RANGE)));
-			List<Entity> entities = this.level().getEntitiesOfClass(Entity.class, aabb, this::shouldSuck);
-
-			for (Entity entity : entities) {
-				if (entity != this) {
-					double posX = pos.x - entity.getX();
-					double posY = pos.y - entity.getY();
-					double posZ = pos.z - entity.getZ();
-					double len = Math.sqrt(posX * posX + posY * posY + posZ * posZ);
-					double lenn = len / SUCK_RANGE;
-					if (len <= SUCK_RANGE) {
-						double strength = (1 - lenn) * (1 - lenn);
-	                    double power = 0.075 * radius;
-
-	                    Vec3 motion = entity.getDeltaMovement();
-	                    
-	                    double motionX = motion.x() + (posX / len) * strength * power;
-	                    double motionY = motion.y() + (posY / len) * strength * power;
-	                    double motionZ = motion.z() + (posZ / len) * strength * power;
-	                    
-	                    entity.setDeltaMovement(motionX, motionY, motionZ);
+		aabb = AABB.encapsulatingFullBlocks(
+				this.blockPosition().offset(new Vec3i(-attackRange, -attackRange, -attackRange)),
+				this.blockPosition().offset(new Vec3i(attackRange, attackRange, attackRange)));
+		List<LivingEntity> entitiesToAttack = this.level().getEntitiesOfClass(LivingEntity.class, aabb,
+				this::shouldAttack);
+		for (Entity toAttack : entitiesToAttack) {
+			if (toAttack != this) {
+				double posX = pos.x - toAttack.getX();
+				double posY = pos.y - toAttack.getY();
+				double posZ = pos.z - toAttack.getZ();
+				double len = Math.sqrt(posX * posX + posY * posY + posZ * posZ);
+				if (len <= attackRange) {
+					if (toAttack instanceof EnderMan || toAttack instanceof EnderDragon) {
+						toAttack.hurt(this.damageSources().playerAttack(this.player), 10.0F);
+					} else if (toAttack instanceof WitherBoss) {
+						WitherBoss wither = (WitherBoss) toAttack;
+						wither.setTarget(null);
+						toAttack.hurt(this.damageSources().fellOutOfWorld(), 15.0F);
+					} else {
+						toAttack.hurt(this.damageSources().fellOutOfWorld(), 10.0F);
 					}
 				}
 			}
+		}
+		attackRange += 2.0D;
 
-			int attackRange = (int) (radius * 0.95D);
+		if (age % 10 == 0) {
+			Vec3 pos1 = pos;
+			int blockRange = (int) Math.round(attackRange);
 
-			aabb = AABB.encapsulatingFullBlocks(
-					this.blockPosition().offset(new Vec3i(-attackRange, -attackRange, -attackRange)),
-					this.blockPosition().offset(new Vec3i(attackRange, attackRange, attackRange)));
-			List<LivingEntity> entitiesToAttack = this.level().getEntitiesOfClass(LivingEntity.class, aabb,
-					this::shouldAttack);
-			for (Entity toAttack : entitiesToAttack) {
-				if (toAttack != this) {
-					double posX = pos.x - toAttack.getX();
-					double posY = pos.y - toAttack.getY();
-					double posZ = pos.z - toAttack.getZ();
-					double len = Math.sqrt(posX * posX + posY * posY + posZ * posZ);
-					if (len <= attackRange) {
-						toAttack.hurt(toAttack.damageSources().fellOutOfWorld(), 3.0F);
-					}
-				}
-			}
-			attackRange += 2.0D;
+			for (int y = -blockRange; y <= blockRange; y++) {
+				for (int z = -blockRange; z <= blockRange; z++) {
+					for (int x = -blockRange; x <= blockRange; x++) {
+						Vec3 pos2 = new Vec3(x, y, z);
+						Vec3 rPos = pos1.add(pos2);
+						BlockPos blockPos = new BlockPos(Mth.floor(rPos.x), Mth.floor(rPos.y), Mth.floor(rPos.z));
+						BlockState blockState = this.level().getBlockState(blockPos);
+						Block block = blockState.getBlock();
+						double dist = Math.sqrt(pos2.x * pos2.x + pos2.y * pos2.y + pos2.z * pos2.z);
+						if (dist <= attackRange && !blockState.isAir() && !(block instanceof BlockInfinityChest)) {
+							if (!(block instanceof BaseFireBlock)) {
+								this.level().levelEvent(2001, blockPos, Block.getId(blockState));
+							}
+							this.level().setBlockAndUpdate(blockPos, Blocks.AIR.defaultBlockState());
+							this.level().gameEvent(GameEvent.BLOCK_DESTROY, blockPos,
+									GameEvent.Context.of(player, blockState));
 
-			if (age % 10 == 0) {
-				Vec3 pos1 = pos;
-				int blockrange = (int) Math.round(attackRange);
+							if (!this.level().isClientSide()) {
+								List<ItemStack> drops = new ArrayList<>();
+								List<ItemStack> blockDrops = Block.getDrops(blockState, (ServerLevel) this.level(),
+										blockPos, null);
+								if (!blockDrops.isEmpty()) {
+									drops.addAll(blockDrops);
+								} else {
+									ResourceLocation blockKey = BuiltInRegistries.BLOCK.getKey(block);
+									Item blockItem = BuiltInRegistries.ITEM.get(blockKey);
+									drops.add(new ItemStack(blockItem));
+								}
 
-				for (int y = -blockrange; y <= blockrange; y++) {
-					for (int z = -blockrange; z <= blockrange; z++) {
-						for (int x = -blockrange; x <= blockrange; x++) {
-							Vec3 pos2 = new Vec3(x, y, z);
-							Vec3 rPos = pos1.add(pos2);
-							BlockPos blockPos = new BlockPos(Mth.floor(rPos.x), Mth.floor(rPos.y), Mth.floor(rPos.z));
-							BlockState blockState = this.level().getBlockState(blockPos);
-							double dist = Math.sqrt(pos2.x * pos2.x + pos2.y * pos2.y + pos2.z * pos2.z);
-							if (dist <= attackRange && !blockState.isAir()) {
-								Explosion explosion = new Explosion(this.level(), this, this.getX(), this.getY(),
-										this.getZ(), 10.0F, false, Explosion.BlockInteraction.DESTROY,
-										List.of(blockPos));
-								float resist = blockState.getBlock().getExplosionResistance();
-								if (resist <= 10.0F) {
-									if (blockState.canDropFromExplosion(this.level(), blockPos, explosion)) {
-										this.level().destroyBlock(blockPos, true);
-									} else {
-										this.level().destroyBlock(blockPos, false);
+								if (!drops.isEmpty()) {
+									for (ItemStack itemStack : drops) {
+										ItemEntity itemEntity = new ItemEntity(this.level(), blockPos.getX(),
+												blockPos.getY(), blockPos.getZ(), itemStack);
+										itemEntity.setDefaultPickUpDelay();
+										this.level().addFreshEntity(itemEntity);
 									}
 								}
 							}
+
+							this.level().destroyBlock(blockPos, true);
 						}
 					}
 				}
@@ -216,5 +277,4 @@ public class EntityGapingVoid extends Entity {
 			this.remove(RemovalReason.KILLED);
 		}
 	}
-
 }
