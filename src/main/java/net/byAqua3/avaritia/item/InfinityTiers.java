185be743f2e7fadddcf21577cabfc220193e8ecc
Aqua3 <88130177+AquaThree@users.noreply.github.com>
2024-08-02 00:19:15 +0800

    Update InfinityTiers.java

--- src/main/java/net/byAqua3/avaritia/item/InfinityTiers.java
+++ src/main/java/net/byAqua3/avaritia/item/InfinityTiers.java
@@ -4,45 +4,50 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
 
 public enum InfinityTiers implements Tier {
-	
-	INFINITY(9999, 0, 9999.0F, -1.0F, 0),
-	SKULLFIRE(32, 0, 9999.0F, -1.0F, 0);
-	
+
+	INFINITY(9999, 0, 9999.0F, -1.0F, 0);
+
 	private final int level;
-    private final int uses;
-    private final float speed;
-    private final float damage;
-    private final int enchantmentValue;
+	private final int uses;
+	private final float speed;
+	private final float damage;
+	private final int enchantmentValue;
 
 	private InfinityTiers(int level, int uses, float speed, float damage, int enchantmentValue) {
-        this.level = level;
-        this.uses = uses;
-        this.speed = speed;
-        this.damage = damage;
-        this.enchantmentValue = enchantmentValue;
-    }
+		this.level = level;
+		this.uses = uses;
+		this.speed = speed;
+		this.damage = damage;
+		this.enchantmentValue = enchantmentValue;
+	}
 
-    public int getUses() {
-        return this.uses;
-    }
+	@Override
+	public int getUses() {
+		return this.uses;
+	}
 
-    public float getSpeed() {
-        return this.speed;
-    }
+	@Override
+	public float getSpeed() {
+		return this.speed;
+	}
 
-    public float getAttackDamageBonus() {
-        return this.damage;
-    }
+	@Override
+	public float getAttackDamageBonus() {
+		return this.damage;
+	}
 
-    public int getLevel() {
-        return this.level;
-    }
+	@Override
+	public int getLevel() {
+		return this.level;
+	}
 
-    public int getEnchantmentValue() {
-        return this.enchantmentValue;
-    }
+	@Override
+	public int getEnchantmentValue() {
+		return this.enchantmentValue;
+	}
 
-    public Ingredient getRepairIngredient() {
-        return Ingredient.EMPTY;
-    }
+	@Override
+	public Ingredient getRepairIngredient() {
+		return Ingredient.EMPTY;
+	}
 }
