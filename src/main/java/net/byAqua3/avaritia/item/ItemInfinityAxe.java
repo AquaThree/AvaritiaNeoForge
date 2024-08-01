387ea59204b59af008371502ecc96716a39de05b
Aqua3 <88130177+AquaThree@users.noreply.github.com>
2024-08-01 22:37:15 +0800

    Update ItemInfinityAxe.java

--- src/main/java/net/byAqua3/avaritia/item/ItemInfinityAxe.java
+++ src/main/java/net/byAqua3/avaritia/item/ItemInfinityAxe.java
@@ -2,10 +2,12 @@ package net.byAqua3.avaritia.item;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -16,7 +18,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class ItemInfinityAxe extends AxeItem {
@@ -36,23 +38,28 @@ public class ItemInfinityAxe extends AxeItem {
 		return super.onEntityItemUpdate(stack, itemEntity);
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onBlockStartBreak(ItemStack stack, BlockPos pos, Player player) {
 		Level level = player.level();
 		BlockPos blockPos = player.blockPosition();
-		int blockrange = (int) Math.round(8.0D);
+		int blockRange = (int) Math.round(8.0D);
 
-		for (int x = -blockrange; x <= blockrange; x++) {
-			for (int y = -blockrange; y <= blockrange; y++) {
-				for (int z = -blockrange; z <= blockrange; z++) {
-					BlockPos rangePos = new BlockPos(Mth.floor(blockPos.getX() + x), Mth.floor(blockPos.getY() + y),
-							Mth.floor(blockPos.getZ() + z));
-					BlockState rangeState = level.getBlockState(rangePos);
-					Block rangeBlock = rangeState.getBlock();
-					if (rangeBlock.getSoundType(rangeState) == SoundType.WOOD
-							|| rangeBlock.getSoundType(rangeState) == SoundType.GRASS) {
-						level.destroyBlock(rangePos, true);
+		if (!player.isCrouching()) {
+			for (int x = -blockRange; x <= blockRange; x++) {
+				for (int y = (int) -Math.round(32.0D); y <= (int) Math.round(32.0D); y++) {
+					for (int z = -blockRange; z <= blockRange; z++) {
+						BlockPos rangePos = new BlockPos(Mth.floor(blockPos.getX() + x), Mth.floor(blockPos.getY() + y),
+								Mth.floor(blockPos.getZ() + z));
+						BlockState rangeState = level.getBlockState(rangePos);
+						Block rangeBlock = rangeState.getBlock();
+						List<TagKey<Block>> tags = rangeState.getTags().toList();
+						if (!rangeState.isAir()) {
+							if (rangeBlock == Blocks.GRASS_BLOCK) {
+								level.setBlockAndUpdate(rangePos, Blocks.DIRT.defaultBlockState());
+							} else if (tags.contains(BlockTags.MINEABLE_WITH_AXE) || tags.contains(BlockTags.LEAVES)) {
+								level.destroyBlock(rangePos, true);
+							}
+						}
 					}
 				}
 			}
@@ -60,34 +67,44 @@ public class ItemInfinityAxe extends AxeItem {
 		return super.onBlockStartBreak(stack, pos, player);
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
 		ItemStack stack = player.getItemInHand(hand);
 		BlockPos blockPos = player.blockPosition();
 		if (player.isCrouching()) {
-			int blockrange = (int) Math.round(8.0D);
+			int blockRange = (int) Math.round(8.0D);
 
 			List<ItemStack> drops = new ArrayList<>();
 
-			for (int x = -blockrange; x <= blockrange; x++) {
-				for (int y = -blockrange; y <= blockrange; y++) {
-					for (int z = -blockrange; z <= blockrange; z++) {
+			for (int x = -blockRange; x <= blockRange; x++) {
+				for (int y = -blockRange; y <= blockRange; y++) {
+					for (int z = -blockRange; z <= blockRange; z++) {
 						BlockPos rangePos = new BlockPos(Mth.floor(blockPos.getX() + x), Mth.floor(blockPos.getY() + y),
 								Mth.floor(blockPos.getZ() + z));
 						BlockState rangeState = level.getBlockState(rangePos);
 						Block rangeBlock = rangeState.getBlock();
-						ResourceLocation blockKey = BuiltInRegistries.BLOCK.getKey(rangeBlock);
-						if (rangeBlock.getSoundType(rangeState) == SoundType.WOOD
-								|| rangeBlock.getSoundType(rangeState) == SoundType.GRASS) {
-							Item blockItem = BuiltInRegistries.ITEM.get(blockKey);
-							drops.add(new ItemStack(blockItem));
-							level.destroyBlock(rangePos, false);
+						List<TagKey<Block>> tags = rangeState.getTags().toList();
+						if (!rangeState.isAir()) {
+							if (rangeBlock == Blocks.GRASS_BLOCK) {
+								level.setBlockAndUpdate(rangePos, Blocks.DIRT.defaultBlockState());
+							} else if (tags.contains(BlockTags.MINEABLE_WITH_AXE) || tags.contains(BlockTags.LEAVES)) {
+								if (!level.isClientSide() && !player.isCreative()) {
+									List<ItemStack> blockDrops = Block.getDrops(rangeState, (ServerLevel) level, blockPos, null);
+									if(!blockDrops.isEmpty()) {
+										drops.addAll(blockDrops);
+									} else {
+										ResourceLocation blockKey = BuiltInRegistries.BLOCK.getKey(rangeBlock);
+										Item blockItem = BuiltInRegistries.ITEM.get(blockKey);
+										drops.add(new ItemStack(blockItem));
+									}
+								}
+								level.destroyBlock(rangePos, false);
+							}
 						}
 					}
 				}
 			}
-			if (!level.isClientSide()) {
+			if (!level.isClientSide() && !drops.isEmpty()) {
 				ItemEntity itemEntity = new ItemEntity(level, blockPos.getX(), blockPos.getY(), blockPos.getZ(),
 						ItemMatterCluster.makeCluster(drops));
 				itemEntity.setDefaultPickUpDelay();
